50f04b510405f648733f968620319bc05807d714
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-01-09 16:36:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function palindrome(str){
- var remove = /[\W_]/g;
-  var lowRegStr = str.toLowerCase().replace(remove, '');
-  var reverseStr = lowRegStr.split('').reverse().join(''); 
-  return reverseStr === lowRegStr;
+function palindrome(string){
+  // optimized approach O(N) Time and O(1) Space
+  let lowerCased = string.toLowerCase();
+
+  let startingPointer = 0;
+  let endingPointer = lowerCased.length -1;
+
+  // palindrome is same as forward and backwards thus checking first character and last character and repeating simultaneously for other part of the strings.
+  while(startingPointer < endingPointer){
+    let currentStartingCharacter = lowerCased[startingPointer];
+    let currentEndingCharacter = lowerCased[endingPointer];
+     
+    if(currentStartingCharacter != currentEndingCharacter) return false;
+     
+    startingPointer++;
+    endingPointer--;
+  }
+
+  return true;
 }
-palindrome("racecar", "RaceCar","raceCAR");
 module.exports = palindrome
+Footer
