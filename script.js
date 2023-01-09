4847d2047eeae9700c5f055a6ef342e8f1e3c911
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-01-09 16:31:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
 function palindrome(str){
-
+ var remove = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(remove, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr;
 }
+palindrome("racecar", "RaceCar","raceCAR");
 module.exports = palindrome
